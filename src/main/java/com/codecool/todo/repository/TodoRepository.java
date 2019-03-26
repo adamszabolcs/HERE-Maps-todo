3fbff693fee89314a268d3866325f97cb77791ff
adamszabolcs <szabolcs.adam91@gmail.com>
2019-03-26 22:51:33 +0100

    jpaRepository added

--- src/main/java/com/codecool/todo/repository/TodoRepository.java
+++ src/main/java/com/codecool/todo/repository/TodoRepository.java
@@ -1,4 +1,7 @@
 package com.codecool.todo.repository;
 
-public class TodoRepository {
+import com.codecool.todo.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
