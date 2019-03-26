8cb6f6ade391b90b14006caceef733da828e7360
adamszabolcs <szabolcs.adam91@gmail.com>
2019-03-26 13:02:52 +0100

    renamed, refactored for single responsibility principle

--- src/main/resources/public/js/dom.js
+++ src/main/resources/public/js/dom.js
@@ -1,13 +1,5 @@
-let showmap = {
-    _center: {
-        lat: '',
-        lng: '',
-    },
+let dom = {
 
-    _app_id: "Hcu7cDJnXgzkmNhIrujH",
-    _app_code: "rJFx0NRya2Y5FdWQehEDkg",
-
-    _error: null,
 
     appendToElement: function (elementToExtend, textToAppend, prepend = false) {
         let fakeDiv = document.createElement('div');
