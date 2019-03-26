3e37988e8de23fbbcaf644462d8cbc393c92d2a1
adamszabolcs <szabolcs.adam91@gmail.com>
2019-03-26 22:50:55 +0100

    /list route added, json returned for frontend

--- src/main/java/com/codecool/todo/controller/WebController.java
+++ src/main/java/com/codecool/todo/controller/WebController.java
@@ -1,22 +1,32 @@
 package com.codecool.todo.controller;
 
+import com.codecool.todo.model.Todo;
+import com.codecool.todo.repository.TodoRepository;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @Slf4j
 @CrossOrigin
 public class WebController {
+
+    @Autowired
+    TodoRepository todoRepository;
 
     @Value("${app.id}")
     private String appId;
@@ -32,9 +42,19 @@ public class WebController {
         return jsonString;
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello!!!!";
+    @GetMapping("/list")
+    public String getList() throws JSONException {
+        List<Todo> todos = todoRepository.findAll();
+        JSONArray arr = new JSONArray();
+        for (Todo todo : todos) {
+            JSONObject jo = new JSONObject();
+            jo.put("title", todo.getTitle());
+            jo.put("latitude", todo.getLatitude());
+            jo.put("longitude", todo.getLongitude());
+            arr.put(jo);
+        }
+        return arr.toString(2);
+
     }
 
 }
