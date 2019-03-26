804df070cd4d9831ce270fc841d7bab0a47d0c9a
adamszabolcs <szabolcs.adam91@gmail.com>
2019-03-26 22:51:13 +0100

    entity created for database

--- src/main/java/com/codecool/todo/model/Todo.java
+++ src/main/java/com/codecool/todo/model/Todo.java
@@ -1,4 +1,31 @@
 package com.codecool.todo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigInteger;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Todo {
+
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    private String title;
+
+    private Double latitude;
+
+    private Double longitude;
+
+
 }
