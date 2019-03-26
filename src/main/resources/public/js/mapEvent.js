e9c90e8551ad5fe2d90ba3684a4ad1c761216c9f
adamszabolcs <szabolcs.adam91@gmail.com>
2019-03-26 13:07:45 +0100

    added dbltap event, create new marker on event

--- src/main/resources/public/js/mapEvent.js
+++ src/main/resources/public/js/mapEvent.js
@@ -1,11 +1,11 @@
 let mapEvent = {
 
     dblTap: function () {
-        let mapEvents = new H.mapevents.MapEvents(hereMap._map);
-
-        hereMap._map.addEventListener('tap', function(evt) {
-            console.log(evt.type, evt.currentPointer.type);
-        })
+        hereMap._map.addEventListener('dbltap', function(evt) {
+            let coord = hereMap._map.screenToGeo(evt.currentPointer.viewportX,
+                evt.currentPointer.viewportY);
+            hereMap.addMarker(coord.lat, coord.lng);
+        });
     }
 
 };
