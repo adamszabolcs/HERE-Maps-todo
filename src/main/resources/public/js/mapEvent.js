e1c491054f84494af302eb44bd721d98b6d99954
adamszabolcs <szabolcs.adam91@gmail.com>
2019-03-26 23:47:09 +0100

    sendToBackend method sends todo to backend database

--- src/main/resources/public/js/mapEvent.js
+++ src/main/resources/public/js/mapEvent.js
@@ -5,7 +5,19 @@ let mapEvent = {
             let coord = hereMap._map.screenToGeo(evt.currentPointer.viewportX,
                 evt.currentPointer.viewportY);
             hereMap.createMarker(coord.lat, coord.lng);
+            mapEvent.sendToBackend(coord);
         });
-    }
+    },
+
+    sendToBackend: function(coord) {
+        fetch("/todo", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(coord),
+        })
+            .then(response => console.log("success: " + JSON.stringify(response)))
+    },
 
 };
