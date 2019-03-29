7ac81013f815cdf88621ea2407acc2f04e3c11f0
adamszabolcs <szabolcs.adam91@gmail.com>
2019-03-29 13:07:13 +0100

    on dblTap, modal shows up, saves todo to backend with title

--- src/main/resources/public/js/mapEvent.js
+++ src/main/resources/public/js/mapEvent.js
@@ -1,14 +1,28 @@
 let mapEvent = {
 
+    _coord: null,
+
+    openModal: function () {
+        $('#exampleModal').on('show.bs.modal', function (event) {
+            $('#exampleModal').modal('show');
+        })
+    },
+
+    sendInfoFromModal: function () {
+        let todoTitle = document.getElementById("message-text").value;
+        hereMap.createMarker(mapEvent._coord.lat, mapEvent._coord.lng, todoTitle);
+        communication.sendTodoToBackend(mapEvent._coord, todoTitle);
+        $('#exampleModal').modal('hide');
+    },
+
     dblTap: function () {
-        hereMap.map._map.addEventListener('dbltap', function(evt) {
-            let coord = hereMap.map._map.screenToGeo(evt.currentPointer.viewportX,
+        hereMap.map._map.addEventListener('dbltap', function (evt) {
+            mapEvent._coord = hereMap.map._map.screenToGeo(evt.currentPointer.viewportX,
                 evt.currentPointer.viewportY);
-            let title = "This is a marker";
-            hereMap.createMarker(coord.lat, coord.lng, title);
-            communication.sendTodoToBackend(coord, title);
+            $('#exampleModal').modal('show');
         });
     },
+
 
     addBubbleInfo: function () {
         hereMap.map._group.addEventListener('tap', function (evt) {
