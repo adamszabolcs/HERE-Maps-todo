a0cf9dd73ead9995a3f6d22e314d7dfc5cf68ad5
adamszabolcs <szabolcs.adam91@gmail.com>
2019-04-01 11:39:18 +0200

    deleted unnecessary not jackson json imports, list returns as list

--- src/main/java/com/codecool/todo/controller/WebController.java
+++ src/main/java/com/codecool/todo/controller/WebController.java
@@ -2,15 +2,9 @@ package com.codecool.todo.controller;
 
 import com.codecool.todo.model.Todo;
 import com.codecool.todo.repository.TodoRepository;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -45,17 +39,9 @@ public class WebController {
     }
 
     @GetMapping("/list")
-    public String getList() throws JSONException {
+    public List<Todo> getList() {
         List<Todo> todos = todoRepository.findAll();
-        JSONArray arr = new JSONArray();
-        for (Todo todo : todos) {
-            JSONObject jo = new JSONObject();
-            jo.put("title", todo.getTitle());
-            jo.put("latitude", todo.getLatitude());
-            jo.put("longitude", todo.getLongitude());
-            arr.put(jo);
-        }
-        return arr.toString(2);
+        return todos;
     }
 
     @PostMapping(value="/todo", headers = "Accept=application/json")
