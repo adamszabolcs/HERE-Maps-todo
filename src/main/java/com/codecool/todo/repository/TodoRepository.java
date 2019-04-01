68c15af5c9f81cfb9739c6ee1fec8e8496ada8c6
adamszabolcs <szabolcs.adam91@gmail.com>
2019-04-01 15:00:21 +0200

    created findAllByStatus query method

--- src/main/java/com/codecool/todo/repository/TodoRepository.java
+++ src/main/java/com/codecool/todo/repository/TodoRepository.java
@@ -1,7 +1,13 @@
 package com.codecool.todo.repository;
 
+import com.codecool.todo.model.Status;
 import com.codecool.todo.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    List<Todo> findAllByStatus(Status status);
+
 }
